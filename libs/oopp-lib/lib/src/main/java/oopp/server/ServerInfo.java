0104f642b2fdca0757ac050df8911da8e216b6cf
siggelicious <siggerajamae@gmail.com>
2023-11-25 00:16:57 +0100

    implemented ServerInfo

--- libs/oopp-lib/lib/src/main/java/oopp/server/ServerInfo.java
+++ libs/oopp-lib/lib/src/main/java/oopp/server/ServerInfo.java
@@ -1,7 +1,18 @@
 package oopp.server;
 
-import java.io.Serializable;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 
-public class ServerInfo implements Serializable {
+import java.net.InetSocketAddress;
 
+@JsonDeserialize
+public record ServerInfo(
+        String name,
+        InetSocketAddress socketAddress) {
+    public ServerInfo(
+            @JsonProperty("name") String name,
+            @JsonProperty("socketAddress") InetSocketAddress socketAddress) {
+        this.name = name;
+        this.socketAddress = socketAddress;
+    }
 }
