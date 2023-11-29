f9b704287afc020d2e346a3ed6437bb98238936c
Zazmuz <rasmuswario@gmail.com>
2023-11-29 05:15:34 +0100

    small fix

--- webserver/app/src/main/java/webserver/routes/FileListRoute.java
+++ webserver/app/src/main/java/webserver/routes/FileListRoute.java
@@ -18,6 +18,6 @@ public class FileListRoute extends Route {
     @Override
     protected void get(HttpExchange exchange) {
         System.out.println("FileListRoute");
-        this.serializeAndWrite(exchange, 200, new String[]{"test 100mb today", "test2 5kb yesterday"});
+        this.serializeAndWrite(exchange, 200, new String[]{"test today 100mb", "test2 yesterday 5kb", "test3 yoday 1gb", "test4 today 1pb"});
     }
 }
