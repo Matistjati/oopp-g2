cd61411078481d441cb022c3f79a5d04abeecd21
Zazmuz <rasmuswario@gmail.com>
2023-12-07 13:49:29 +0100

    Integrated to the new subsystem

--- landrive/fileserver/src/main/java/landrive/fileserver/handler/filedownload/FileDownloadHandler.java
+++ landrive/fileserver/src/main/java/landrive/fileserver/handler/filedownload/FileDownloadHandler.java
@@ -1,42 +1,29 @@
 package landrive.fileserver.handler.filedownload;
 
 import io.vertx.core.Handler;
-import io.vertx.core.http.HttpHeaders;
 import io.vertx.core.http.HttpServerResponse;
+import io.vertx.ext.web.Router;
 import io.vertx.ext.web.RoutingContext;
 import landrive.fileserver.filesystem.FsService;
+import landrive.lib.route.MountingHandlers;
 
 public class FileDownloadHandler implements Handler<RoutingContext> {
     private final FsService fsService;
 
-    public FileDownloadHandler(FsService fsService) {
+    public FileDownloadHandler(final FsService fsService) {
         this.fsService = fsService;
     }
-
     @Override
     public void handle(RoutingContext ctx) {
         String fileName = ctx.request().getParam("fileName");
-        //final String dir = ctx.pathParam("dir");
-
-        System.out.println("Downloading file: " + fileName);
-        //System.out.println("From directory: " + dir);
-
+        String directory = ctx.request().getParam("directory");
+        System.out.println("FileDownloadHandler: " + fileName + " " + directory);
+        if (directory == null) {
+            directory = "";
+        }
 
         HttpServerResponse response = ctx.response();
 
-        response.putHeader(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + fileName);
-        response.putHeader(HttpHeaders.CONTENT_TYPE, "application/octet-stream");
-
-        ctx.response().sendFile("./storage/" + fileName,
-                ar -> {
-                    if (ar.succeeded()) {
-                        System.out.println("File downloaded successfully");
-                        ctx.response().setStatusCode(200).end();
-                    } else {
-                        System.out.println("File download failed");
-                        ar.cause().printStackTrace();
-                        ctx.response().setStatusCode(500).end("Internal Server Error");
-                    }
-                });
+        fsService.downloadFile(fileName, directory, response).onFailure(ctx::fail);
     }
 }
